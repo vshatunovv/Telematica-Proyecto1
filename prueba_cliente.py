a1c472738915c6a24d3cd033b41a9b65ef3d3453
saradrl <67977092+saradrl@users.noreply.github.com>
2024-04-24 04:17:53 +0000

    Update prueba_cliente.py

--- prueba_cliente.py
+++ prueba_cliente.py
@@ -7,6 +7,12 @@ def registrar_log(log_file, query, response_ip):
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         log.write(f"{timestamp} {query} {response_ip}\n")
 
+def parse_ip_address(response_bytes):
+    # La respuesta_bytes puede contener múltiples direcciones IP
+    # Pero para este caso, supondremos que solo hay una dirección IP
+    ip_parts = [str(byte) for byte in response_bytes]
+    return '.'.join(ip_parts)
+
 def main():
     if len(sys.argv) != 2:
         print("Uso: python3 DNSCLIENTE.py </path/log.log>")
@@ -31,8 +37,11 @@ def main():
 
         print("Respuesta del servidor DNS:", response)
         
+        # Convertir los bytes de la respuesta a una dirección IP legible
+        response_ip = parse_ip_address(response)
+        
         # Registrar la transacción en el archivo de log del cliente
-        registrar_log(log_file, query, response)
+        registrar_log(log_file, query, response_ip)
     except Exception as e:
         print("Error:", e)
     finally:
